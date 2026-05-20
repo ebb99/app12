bc4bd7b6bc6f8f769b71a96c52f57c44762164ba
Eduard Ballweg <eduard.ballweg@gmail.com>
2026-05-20 19:08:20 +0200

    asc

--- server.js
+++ server.js
@@ -487,7 +487,7 @@ app.get("/api/spiele", requireLogin, async (req, res) => {
             LEFT JOIN tips t
               ON t.spiel_id = s.id  
              AND t.user_id = $1
-            ORDER BY s.anstoss DESC
+            ORDER BY s.anstoss asc
         `, [userId]);
 
         res.json(result.rows);
